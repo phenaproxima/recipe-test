e083f4bac404f4a178a47f1045fb91377b32a6d6
Phéna Proxima <adam@phenaproxima.net>
2023-12-12 14:41:36 -0500

    Disable UnknownPathExcluder since it interferes with local patches.

--- web/modules/custom/project_browser_recipe_demo/src/ProjectBrowserRecipeDemoServiceProvider.php
+++ web/modules/custom/project_browser_recipe_demo/src/ProjectBrowserRecipeDemoServiceProvider.php
@@ -4,6 +4,7 @@ namespace Drupal\project_browser_recipe_demo;
 
 use Drupal\Core\DependencyInjection\ContainerBuilder;
 use Drupal\Core\DependencyInjection\ServiceProviderBase;
+use Drupal\package_manager\PathExcluder\UnknownPathExcluder;
 use Drupal\package_manager\Validator\ComposerPluginsValidator;
 
 class ProjectBrowserRecipeDemoServiceProvider extends ServiceProviderBase {
@@ -17,6 +18,11 @@ class ProjectBrowserRecipeDemoServiceProvider extends ServiceProviderBase {
     // Package Manager's Composer plugins validator doesn't allow the 2.0.x
     // line of cweagans/composer-patches.
     $container->getDefinition(ComposerPluginsValidator::class)
+      ->clearTag('event_subscriber');
+
+    // The unknown path excluder causes patch information to be removed from
+    // the stage directory.
+    $container->getDefinition(UnknownPathExcluder::class)
       ->clearTag('event_subscriber');
   }
 
