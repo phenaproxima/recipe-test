5f932ad0f0ad7d2b831a41ba30a3075fb143dfeb
Phéna Proxima <adam@phenaproxima.net>
2023-12-14 11:00:04 -0500

    Disable admin_toolbar suite for now.

--- src/Scripts.php
+++ src/Scripts.php
@@ -14,6 +14,14 @@ final class Scripts {
     if (file_exists($path)) {
       $data = file_get_contents($path);
       $data = Yaml::decode($data);
+      $data['install'] = array_diff($data['install'], [
+        'admin_toolbar',
+        'admin_toolbar_search',
+        'admin_toolbar_tools',
+      ]);
+      unset($data['config']['import']['admin_toolbar']);
+      unset($data['config']['import']['admin_toolbar_search']);
+      unset($data['config']['import']['admin_toolbar_tools']);
       unset($data['config']['actions']);
       file_put_contents($path, Yaml::encode($data));
     }
