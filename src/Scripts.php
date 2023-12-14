78a09d864de616fc643cd2a51ca370613f3056d6
Phéna Proxima <adam@phenaproxima.net>
2023-12-14 10:52:31 -0500

    Switch to Drupal core 10.2.x-dev.

--- src/Scripts.php
+++ src/Scripts.php
@@ -3,31 +3,8 @@
 namespace phen;
 
 use Drupal\Component\Serialization\Yaml;
-use Symfony\Component\Finder\Finder;
 
 final class Scripts {
-
-  /**
-   * Makes all info files compatible with Drupal 11.
-   */
-  public static function rewriteInfoFiles(): void {
-    $finder = Finder::create()
-      ->in('web')
-      ->name('*.info.yml');
-
-    /** @var \Symfony\Component\Finder\SplFileInfo $file */
-    foreach ($finder as $file) {
-      $data = Yaml::decode($file->getContents());
-      if (empty($data['core_version_requirement'])) {
-        continue;
-      }
-      if (str_contains($data['core_version_requirement'], ' || ^11')) {
-        continue;
-      }
-      $data['core_version_requirement'] .= ' || ^11';
-      file_put_contents($file->getPathname(), Yaml::encode($data));
-    }
-  }
 
   /**
    * Changes the gin-admin-experience recipe to enable testing.
